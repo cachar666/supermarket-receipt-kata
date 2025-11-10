b1d6c56767a3ab5cc5c699fb8babde1cc4502b92
Carlos Chaves <cachar@gmail.com>
2025-11-09 21:14:46 -0500

    😖 De Cero

--- obj/Debug/net9.0/supermarket-receipt-kata.AssemblyInfo.cs
+++ obj/Debug/net9.0/supermarket-receipt-kata.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("supermarket-receipt-kata")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd611153ec64791c897034a62383334b14819d75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a39db66c2e49a8d062cea618aa8dae7a896d1a22")]
 [assembly: System.Reflection.AssemblyProductAttribute("supermarket-receipt-kata")]
 [assembly: System.Reflection.AssemblyTitleAttribute("supermarket-receipt-kata")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
